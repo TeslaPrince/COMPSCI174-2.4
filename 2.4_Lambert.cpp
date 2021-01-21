1459f70709e35ad7f62f863bbfb27fdbfcb436d4
Tesla Prince <57016635+TeslaPrince@users.noreply.github.com>
2021-01-21 00:37:48 -0600

    Add files via upload

---
+++ 2.4_Lambert.cpp
@@ -0,0 +1,10 @@
+//2-3-2019
+//Name, Address, Phone Number
+#include <iostream>
+using namespace std;
+int main()
+{
+cout<<"Jenna Christine Lambert"<<endl;
+cout<<"251 Hill Ct."
+cout<<"West Chicago, IL 60185"<<endl;
+cout<<"1(630)-251-3195"<<endl;
